198e8efd82114651283c59fe02a85c0cea6e0ede
kennuu <maribor23072006@gmail.com>
2023-03-27 00:41:30 +0300

    added show animation for scroll top btn

--- src/Pages/Portfolio/Containers/ScrollTop.jsx
+++ src/Pages/Portfolio/Containers/ScrollTop.jsx
@@ -1,37 +1,34 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { arrowTopSVG } from "../../../../public/Portfolio/Svgs";
-import SmallButton from "../Components/SmallButton";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { arrowTopSVG } from '../../../../public/Portfolio/Svgs'
+import SmallButton from '../Components/SmallButton'
 
 export default function ScrollTop(props) {
-  const [showBtn, setShow] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      const topOffset = props.blockRefs[1].offsetTop -10;
-      const scrollTop = document.documentElement.scrollTop;
-      setShow(true && scrollTop > topOffset);
-    };
+	const [showBtn, setShow] = useState(false)
 
-    window.addEventListener("scroll", handleScroll);
+	useEffect(() => {
+		const handleScroll = () => {
+			const topOffset = props.blockRefs[1].offsetTop - 10
+			const scrollTop = document.documentElement.scrollTop
+			setShow(scrollTop > topOffset)
+		}
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+		window.addEventListener('scroll', handleScroll)
 
-  return (
-    showBtn && (
-      <SmallButton
-        onClick={() => {
-          props.blockRefs[0].scrollIntoView({ behavior: "smooth" });
-        }}
-        aria-label="Scroll to top"
-        className={
-          " fixed right-[1.6rem] bottom-[60vh] z-10 dark:text-white pc:right-[4vh] pc:bottom-[4vh] "
-        }
-      >
-        {arrowTopSVG}
-      </SmallButton>
-    )
-  );
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
+
+	return (
+		<SmallButton
+			onClick={() => {
+				props.blockRefs[0].scrollIntoView({ behavior: 'smooth' })
+			}}
+			aria-label='Scroll to top'
+			className={`fixed pc:bottom-[4vh] right-[1.6rem] bottom-[60vh] pc:right-[4vh] ${showBtn ? ' translate-y-0 ' : ' pc:translate-y-20 translate-y-[100vh] '}`}
+		>
+			{arrowTopSVG}
+		</SmallButton>
+	)
 }
--- src/Pages/Portfolio/Containers/ThemeSwitcher.jsx
+++ src/Pages/Portfolio/Containers/ThemeSwitcher.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { sunSVG, moonSVG } from "../../../../public/Portfolio/Svgs";
-import SmallButton from "../Components/SmallButton";
-import { useState, useEffect } from "react";
+import React from 'react'
+import { sunSVG, moonSVG } from '../../../../public/Portfolio/Svgs'
+import SmallButton from '../Components/SmallButton'
+/* import { useState, useEffect } from 'react' */
 
 export default function ThemeSwitcher(props) {
-  const [showBtn, setShow] = useState(false);
+	/* const [showBtn, setShow] = useState(false); */
 
-  const setTheme = (themeName) => {
-    themeName === "dark"
-      ? document.documentElement.classList.add("dark")
-      : document.documentElement.classList.remove("dark");
-  };
+	const setTheme = (themeName) => {
+		themeName === 'dark'
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark')
+	}
 
-  const themeSwitch = () => {
-    document.documentElement.classList.contains("dark")
-      ? setTheme("light")
-      : setTheme("dark");
-  };
+	const themeSwitch = () => {
+		document.documentElement.classList.contains('dark')
+			? setTheme('light')
+			: setTheme('dark')
+	}
 
-  useEffect(() => {
+	/* useEffect(() => {
     const handleScroll = () => {
       const topOffset = props.blockRefs[1].offsetTop -10;
       const scrollTop = document.documentElement.scrollTop;
@@ -30,23 +30,19 @@ export default function ThemeSwitcher(props) {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [props.topRef]);
+  }, [props.topRef]); */
 
-  return (
-    showBtn && (
-      <SmallButton
-        className=" fixed top-[8vh] right-[1.6rem] z-10 dark:text-white pc:top-[4vh] pc:right-[4vh] "
-        onClick={() => themeSwitch()}
-        role="switch"
-        aria={`Switch Theme to ${
-          document.documentElement.classList.contains("dark")
-            ? " light"
-            : " dark"
-        }`}
-      >
-        {sunSVG}
-        {moonSVG}
-      </SmallButton>
-    )
-  );
+	return (
+		<SmallButton
+			className=' fixed top-[1.6rem] right-[1.6rem] z-10 dark:text-white pc:top-[4vh] pc:right-[4vh] '
+			onClick={() => themeSwitch()}
+			role='switch'
+			aria={`Switch Theme to ${
+				document.documentElement.classList.contains('dark') ? ' light' : ' dark'
+			}`}
+		>
+			{sunSVG}
+			{moonSVG}
+		</SmallButton>
+	)
 }
